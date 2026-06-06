a5389802000be3d5f57c251bebeadbbed06128ad
igprad01 <igustiputra99@gmail.com>
2026-06-06 21:03:05 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+  },
 };
 
 export default nextConfig;
